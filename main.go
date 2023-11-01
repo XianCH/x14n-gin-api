b36afdde90cc3568a4d05f5bc3a4c0dbb5ca653b
XianCH <1160698903@qq.com>
2023-11-01 18:06:44 +0800

    main

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"github.com/x14n/x14n-gin-api/global"
 	"github.com/x14n/x14n-gin-api/initliza"
+	"github.com/x14n/x14n-gin-api/server"
 )
 
 func main() {
-	// initliza.InitLog()
-	// global.GLogger.Sugar().Infof("日志写入测试:%v", strings.Repeat("hello", 6))
-	// global.GLogger.Info("Info记录", zap.String("name", "张三"))
-	initliza.InitLoger2("./config", "debug")
-	initliza.InitGorm()
-	global.GLogger.Info("server start")
-	// current := time.Now()
-	// fmt.Println(current)
-	// formattime := current.Format("2006-01-02/15:04:05")
-	// fmt.Println("current:", formattime)
+	initliza.InitServer()
+
+	server.RunServer()
+
 }
